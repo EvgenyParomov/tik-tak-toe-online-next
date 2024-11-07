ae08f59e5dfd6d3393ad89d0c4226680182f8101
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-11-07 20:04:15 +0300

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 import { Card, CardTitle } from "@/shared/ui/card";
 
-export  default async function Home() {
-
+export default async function Home() {
   const games = await prisma.game.findMany();
 
-
-
   return (
-    <div >
-     <Button size="lg" variant={'destructive'}>Hellow</Button>
+    <div>
+      <Button size="lg" variant={"destructive"}>
+        Hellow
+      </Button>
 
-     {games.map(game => {
-      return <Card key={game.id}>
-        <CardTitle>{game.name}</CardTitle>
-      </Card>
-     })}
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
