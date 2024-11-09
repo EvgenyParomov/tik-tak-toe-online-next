9a356f986ee30b13d0f72d36d2dba1dcd94ef76e
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-11-09 22:40:41 +0300

    feat: update player

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.create({
-    data: { login: "user", passwordHash: "as;dflkjas;df", rating: 1000 },
-  });
-  const user2 = await prisma.user.create({
-    data: { login: "user2", passwordHash: "as;dflkjas;df", rating: 800 },
-  });
-  await prisma.game.create({
-    data: {
-      field: Array(9).fill(null),
-      status: "idle",
-      players: {
-        connect: {
-          id: user.id,
-        },
-      },
-    },
-  });
-  await prisma.game.create({
-    data: {
-      field: Array(9).fill(null),
-      status: "idle",
-      players: {
-        connect: {
-          id: user2.id,
-        },
-      },
-    },
-  });
-}
+async function main() {}
 main()
   .then(async () => {
     await prisma.$disconnect();
--- src/entities/game/repositories/game.ts
+++ src/entities/game/repositories/game.ts
@@ -5,19 +5,22 @@ import {
   GameOverEntity,
   PlayerEntity,
 } from "../domain";
-import { Game, Prisma, User } from "@prisma/client";
+import { Game, GamePlayer, Prisma, User } from "@prisma/client";
 import { z } from "zod";
-import { removePassword } from "@/shared/lib/password";
 import { GameId } from "@/kernel/ids";
+
+const gameInclude = {
+  winner: { include: { user: true } },
+  players: { include: { user: true } },
+};
 
 async function gamesList(where?: Prisma.GameWhereInput): Promise<GameEntity[]> {
   const games = await prisma.game.findMany({
     where,
-    include: {
-      winner: true,
-      players: true,
-    },
+    include: gameInclude,
   });
+
+  games.map((game) => game.players.map((p) => p.user));
 
   return games.map(dbGameToGameEntity);
 }
@@ -28,16 +31,14 @@ async function startGame(gameId: GameId, player: PlayerEntity) {
       where: { id: gameId },
       data: {
         players: {
-          connect: {
-            id: player.id,
+          create: {
+            userId: player.id,
+            index: 1,
           },
         },
         status: "inProgress",
       },
-      include: {
-        winner: true,
-        players: true,
-      },
+      include: gameInclude,
     }),
   );
 }
@@ -45,10 +46,7 @@ async function startGame(gameId: GameId, player: PlayerEntity) {
 async function getGame(where?: Prisma.GameWhereInput) {
   const game = await prisma.game.findFirst({
     where,
-    include: {
-      winner: true,
-      players: true,
-    },
+    include: gameInclude,
   });
   if (game) {
     return dbGameToGameEntity(game);
@@ -63,13 +61,13 @@ async function createGame(game: GameIdleEntity): Promise<GameEntity> {
       id: game.id,
       field: game.field,
       players: {
-        connect: { id: game.creator.id },
+        create: {
+          index: 0,
+          userId: game.creator.id,
+        },
       },
     },
-    include: {
-      players: true,
-      winner: true,
-    },
+    include: gameInclude,
   });
 
   return dbGameToGameEntity(createdGame);
@@ -79,11 +77,13 @@ const fieldSchema = z.array(z.union([z.string(), z.null()]));
 
 function dbGameToGameEntity(
   game: Game & {
-    players: User[];
-    winner?: User | null;
+    players: Array<GamePlayer & { user: User }>;
+    winner?: (GamePlayer & { user: User }) | null;
   },
 ): GameEntity {
-  const players = game.players.map(removePassword);
+  const players = game.players
+    .sort((a, b) => a.index - b.index)
+    .map(dbPlayerToPlayer);
 
   switch (game.status) {
     case "idle": {
@@ -116,10 +116,20 @@ function dbGameToGameEntity(
         players: players,
         status: game.status,
         field: fieldSchema.parse(game.field),
-        winner: removePassword(game.winner),
+        winner: dbPlayerToPlayer(game.winner),
       } satisfies GameOverEntity;
     }
   }
 }
+
+export const dbPlayerToPlayer = (
+  db: GamePlayer & { user: User },
+): PlayerEntity => {
+  return {
+    id: db.user.id,
+    login: db.user.login,
+    rating: db.user.rating,
+  };
+};
 
 export const gameRepository = { gamesList, createGame, getGame, startGame };
